dbb694d8ee4fa53a1fbe17a9f0fc9f9d91d57b53
codingxllama <osamaalchalabi2@gmail.com>
2019-08-02 17:03:31 -0600

    Added a lesson note for different Unary Ops with example

--- Basics/unaryOP_Ex1.c
+++ Basics/unaryOP_Ex1.c
@@ -1,4 +1,15 @@
 //Purpose : To test your understanding of unary increment op.
+/*
+Lesson: 
+
+There are 4 Unary Operators, these include:
+1. Position '+' example => a=+1;
+2. Negative '-' example => a=-1;
+3. Increment (post or pre) '++' example => a++ same as  a+=1;
+4. Decrement (post or pre) '--' example => a-- same as a-=1; 
+
+
+ */
 
 #include<stdio.h>
 
@@ -14,7 +25,7 @@ int main()
     // Pre-incrementing the value of a, and displaying it to the console.
     printf("The value of a, after pre-increment is now: %d\n",++a);
 
-    
+
 
 
 
