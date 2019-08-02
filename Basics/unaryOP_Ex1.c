25a964fba0e9a8d81a3713ba0d5487c82a304476
codingxllama <osamaalchalabi2@gmail.com>
2019-08-02 16:59:15 -0600

    Created unary post and decrement example

---
+++ Basics/unaryOP_Ex1.c
@@ -0,0 +1,23 @@
+//Purpose : To test your understanding of unary increment op.
+
+#include<stdio.h>
+
+int main()
+{
+
+    int a=1;
+    
+    // Post-incrementing the value of a, and displaying it to the console.
+    printf("The value of a is: %d\n",a++);
+    printf("The value of a, after post incrment is now: %d\n",a);
+    
+    // Pre-incrementing the value of a, and displaying it to the console.
+    printf("The value of a, after pre-increment is now: %d\n",++a);
+
+    
+
+
+
+
+    return 0;
+}
