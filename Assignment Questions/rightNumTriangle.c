9bf228ff879a8a20af1397ed02c88f11dfbe5319
codingxllama <osamaalchalabi2@gmail.com>
2019-08-18 21:09:16 -0600

    Created a c program to display right number triangle

---
+++ Assignment Questions/rightNumTriangle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int row;
+    int col;
+    int lineAmount;
+
+    printf("Enter line amount: ");
+    scanf("%d", &lineAmount);
+
+    for (row = 1; row <= lineAmount; row++)
+    {
+        for (col = 1; col <= row; col++)
+        {
+            printf("%2d ", row);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
